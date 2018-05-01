d9ceb6ae841175654d8560cfb321dada70c05f2e
atillatan <atillatan@gmail.com>
2018-05-01 19:20:56 +0300

    pool

--- src/RoboUtil.Examples/ThreadPoolManagerExample.cs
+++ src/RoboUtil.Examples/ThreadPoolManagerExample.cs
@@ -210,7 +210,34 @@ namespace RoboUtil.Examples
             Console.WriteLine(stopwatch.Elapsed);
         }
 
+        public void coreExample()
+        {
+            int toProcess = 10;
+            using (ManualResetEvent resetEvent = new ManualResetEvent(false))
+            {
+                var list = new List<int>();
+                for (int i = 0; i < 10; i++) list.Add(i);
 
+                for (int i = 0; i < 10; i++)
+                {
+                    ThreadPool.QueueUserWorkItem(
+                       new WaitCallback(x =>
+                       {
+                           Console.WriteLine(x);
+                           // Safely decrement the counter
+                           if (Interlocked.Decrement(ref toProcess) == 0)
+                               resetEvent.Set();
+
+                       }), list[i]);
+                }
+
+                resetEvent.WaitOne();
+            }
+            // When the code reaches here, the 10 threads will be done
+            Console.WriteLine("Done");
+
+
+        }
         private static void targetMethod(object obj)
         {
             JobData jobData = (JobData)obj;//we receive JobData from each thread
